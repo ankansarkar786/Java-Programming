f289b247ec7fa83c68c94b3497e916d908cec451
Ankan Sarkar <107130179+ankansarkar786@users.noreply.github.com>
2025-05-02 20:53:49 +0530

    Add files via upload

---
+++ sortColors.java
@@ -0,0 +1,40 @@
+package com.ankan;
+
+public class sortColors {
+	 public void sortColorsf(int[] nums) {
+	        int low = 0;
+	        int mid = 0;
+	        int high = nums.length - 1;
+
+	        while (mid <= high) {
+	            if (nums[mid] == 0) {
+	                int temp = 0;
+	                temp = nums[mid];
+	                nums[mid] = nums[low];
+	                nums[low] = temp;
+	                low++;
+	                mid++;
+	            } else if (nums[mid] == 1) {
+	                mid++;
+	            } else {
+	                int temp = 0;
+	                temp = nums[mid];
+	                nums[mid] = nums[high];
+	                nums[high] = temp;
+	                high--;
+	            }
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        sortColors sol = new sortColors();
+
+	        int[] nums = {2, 0, 2, 1, 1, 0, 2, 1, 0}; // Example input
+	        sol.sortColorsf(nums);
+
+	        // Print the sorted array
+	        for (int num : nums) {
+	            System.out.print(num + " ");
+	        }
+	    }
+}
